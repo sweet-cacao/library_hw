c581d5a521805ae16d36bc591a15d7396af04f08
Айрапетян Анна <amarayrapetyan@sberbank.ru>
2022-08-27 19:20:49 +0300

    added dbref

--- src/main/java/ann/ayrapetyan/hw/jdbc/domain/Book.java
+++ src/main/java/ann/ayrapetyan/hw/jdbc/domain/Book.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
@@ -17,7 +18,10 @@ public class Book {
     @Id
     private long id;
     private String name;
+    @DBRef
     private Genre genre;
+    @DBRef
     private Author author;
+    @DBRef
     private List<BookComment> comments = new ArrayList<>();
 }
